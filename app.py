e3f8a045bb1c1dda51d5bc7fcc2c76599cfbb4fd
BuildTools <unconfigured@null.spigotmc.org>
2020-04-07 15:52:46 +0200

    Test comm,it

--- app.py
+++ app.py
@@ -26,19 +26,19 @@ def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
+            flash('No file part', category='error')
             return redirect(request.url)
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
-            flash('No selected file')
+            flash('No selected file', category='error')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             plot = wastats.process(f'{app.config["UPLOAD_FOLDER"]}/{filename}')
-            #os.remove(f'{app.config["UPLOAD_FOLDER"]}/{filename}')
+            os.remove(f'{app.config["UPLOAD_FOLDER"]}/{filename}')
             return render_template('results.html', name=filename.split('.')[0], source=plot)
     return render_template('index.html')
 
